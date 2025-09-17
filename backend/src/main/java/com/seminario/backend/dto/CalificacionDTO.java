7dbc150f7e70736cddfe98609c074839f70d0457
Juan Cruz Kraft <kraft.juancruz@gmail.com>
2025-09-16 22:48:37 -0300

    endpoint para ver calificaciones de un vendedir

---
+++ backend/src/main/java/com/seminario/backend/dto/CalificacionDTO.java
@@ -0,0 +1,9 @@
+package com.seminario.backend.dto;
+
+public class CalificacionDTO {
+    public Long id;
+    public int puntaje;
+    public String comentario;
+    public String nombreCliente;
+    
+}
---
+++ backend/src/main/java/com/seminario/backend/dto/request/VisualizarCalificacionVendedorRequestDTO.java
@@ -0,0 +1,7 @@
+package com.seminario.backend.dto.request;
+
+public class VisualizarCalificacionVendedorRequestDTO {
+
+    public Long vendedorId;
+    
+}
